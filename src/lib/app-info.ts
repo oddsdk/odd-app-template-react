82bd3141fca123b193ffb68622d603f5f3b593b8
avivash <av@andrewvivash.com>
2022-10-14 14:44:28 -0700

    Fix: change app-info to using ' instead of "

--- src/lib/app-info.ts
+++ src/lib/app-info.ts
@@ -1,5 +1,5 @@
-export const appName = "Awesome Webnative App";
-export const appDescription = "This is another awesome Webnative app.";
-export const appURL = "https://webnative-react.netlify.app";
+export const appName = 'Awesome Webnative App';
+export const appDescription = 'This is another awesome Webnative app.';
+export const appURL = 'https://webnative-react.netlify.app';
 export const appImageURL = `${appURL}/preview.png`;
 export const fissionServerUrl = 'runfission.com';
