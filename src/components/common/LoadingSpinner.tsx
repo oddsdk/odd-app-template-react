a02d180a7a1727ca1e0545b64f74d6cd69f7ad3a
avivash <av@andrewvivash.com>
2022-11-18 18:01:23 -0800

    Feat: add disclaimer for private images in modal

--- src/components/common/LoadingSpinner.tsx
+++ src/components/common/LoadingSpinner.tsx
@@ -1,7 +1,5 @@
 const LoadingSpinner = () => (
-  <div
-    className="loader ease-linear rounded-full border-4 border-t-4 border-neutral border-t-orange-300 h-16 w-16 animate-spin"
-  />
+  <div className="loader ease-linear rounded-full border-4 border-t-4 border-t-orange-500 border-base-content h-16 w-16 animate-spin" />
 );
 
 export default LoadingSpinner;
--- tailwind.config.js
+++ tailwind.config.js
@@ -63,7 +63,7 @@ module.exports = {
       boxShadow: {
         orange: "0px 8px 0px rgba(243, 110, 101, 0.2)",
       },
-      color: {
+      colors: {
         blue: {
           100: "#DBEAFE",
           900: "#1E3A8A",
@@ -75,7 +75,9 @@ module.exports = {
         neutral: {
           50: "#FAFAFA",
           500: "#737373",
+          700: "#404040",
           800: "#262626",
+          900: "#171717",
         },
         orange: {
           50: "#FFF7ED",
